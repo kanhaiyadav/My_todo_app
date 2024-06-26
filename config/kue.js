535f779bd90b8cc0b40369f6cd404cb1ab0af764
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-06-26 22:07:02 +0530

    little changes 2

--- config/kue.js
+++ config/kue.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const redisConfig = {
     redis: {
-        host: REDIS_HOST || '172.31.46.223',  // Default to localhost
-        port: REDIS_PORT || 6379,        // Default Redis port
+        host: process.env.REDIS_HOST || '172.31.46.223',  // Default to localhost
+        port: process.env.REDIS_PORT || 6379,        // Default Redis port
     }
 };
 const kue = require('kue');
