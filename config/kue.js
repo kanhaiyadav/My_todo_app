a5285e7744fcce89cddf01cece9ecdf826df00d5
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-06-26 12:09:17 +0530

    Some minor change 2

--- config/kue.js
+++ config/kue.js
@@ -1,5 +1,12 @@
+const redisConfig = {
+    redis: {
+        host: process.env.REDIS_HOST || '127.0.0.1',  // Default to localhost
+        port: process.env.REDIS_PORT || 6379,        // Default Redis port
+        auth: process.env.REDIS_PASSWORD             // Optional password
+    }
+};
 const kue = require('kue');
 
-const queue = kue.createQueue();
+const queue = kue.createQueue(redisConfig);
 
 module.exports = queue;
